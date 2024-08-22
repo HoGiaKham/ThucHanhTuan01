5da09187937dde6c5082fefe51beb357fdda1c17
unknown <hogiakham1705@gmail.com>
2024-08-22 08:15:16 +0700

    commit

---
+++ Ex01/BMI.js
@@ -0,0 +1,9 @@
+massMark = 78
+heightMark = 1.69
+BMIMark = massMark / (heightMark * heightMark)
+console.log("BMIMark is:", BMIMark)
+
+massJohn = 92
+heightJhon = 1.95
+BMIJohn = massJohn / (heightJhon * heightJhon)
+console.log("BMIJohn is:", BMIJohn)
---
+++ Ex02/CPBMI.js
@@ -0,0 +1,6 @@
+if (BMIMark > BMIJohn) {
+    console.log("Mark's BMI is higher than John's!")
+
+} else {
+    console.log("John's BMI is higher than Mark's!")
+}

dcdae934c587e8ef2af9e40db384817f09944f0a
unknown <hogiakham1705@gmail.com>
2024-08-22 08:48:41 +0700

    first commit

---
+++ Ex03/CP.js
@@ -0,0 +1,7 @@
+if (Dtb > Ktb) {
+    console.log("Dolphins is the winner")
+} else if (Dtb < Ktb) {
+    console.log("Koalahs is the winner")
+} else if (Dtb == Ktb) {
+    console.log("Draw")
+}
---
+++ Ex03/data.js
@@ -0,0 +1,13 @@
+Ds1 = 96
+Ds2 = 108
+Ds3 = 89
+
+Ks1 = 88
+Ks2 = 91
+Ks3 = 110
+
+Dtb = (Ds1 + Ds2 + Ds3) / 3
+console.log(Dtb)
+
+Ktb = (Ks1 + Ks2 + Ks3) / 3
+console.log(Ktb)
